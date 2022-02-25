b356d49830c7fd9aec2728496a8d3ba162beb853
Jacob <jrblock05@gmail.com>
2022-02-25 15:03:17 -0600

    final upload of oojs game

--- oojs_game/assets/scripts/script.js
+++ oojs_game/assets/scripts/script.js
@@ -14,25 +14,58 @@ let levelNum = 1;
 
 let start = false;
 
+let menu = true;
+
+let music = false;
+
+let lose = false;
+
+const backgroundMusic = new Audio('assets/music/background.mp3');
+
+const engineSound = new Audio('assets/music/car_engine.mp3');
+
+backgroundMusic.volume = 0.05;
+
+engineSound.volume = 0.1;
+
 const finish = 1250;
 
 function clicked() {
-    distance += 25;
-    car.left = distance + "px";
-    start = true;
-    if (distance >= finish) {
-        start = false;
-        level += 0.25;
-
-        distance = 0;
-        enemySpeed = 0;
-
-        enemy.left = 0;
-        car.left = 0;
-
-        levelNum += 1;
-
+    if (lose == true) {
+        lose = false;
         levelText.innerText = "Level: " + levelNum;
+    }
+
+    if (music == false) {
+        backgroundMusic.play();
+        music = true;
+    }
+
+    if (menu == true) {
+        document.getElementById("clickButton").value = "Rapidly Click Here";
+        menu = false;
+        engineSound.play();
+        return;
+    }
+
+    if (menu == false) {
+        distance += 25;
+        car.left = distance + "px";
+        start = true;
+        if (distance >= finish) {
+            start = false;
+            level += 0.25;
+
+            distance = 0;
+            enemySpeed = 0;
+
+            enemy.left = 0;
+            car.left = 0;
+
+            levelNum += 1;
+
+            levelText.innerText = "Level: " + levelNum;
+        }
     }
 }
 
@@ -45,9 +78,8 @@ function enemyMove() {
     }
 
     if (enemySpeed >= finish) {
-        document.getElementById("clickButton").disabled = true;
-
         start = false;
+        menu = true;
 
         distance = 0;
         enemySpeed = 0;
@@ -55,6 +87,11 @@ function enemyMove() {
         enemy.left = 0;
         car.left = 0;
 
+        levelNum = 1;
+        level = 1;
+
         levelText.innerText = "You Lost on level " + levelNum;
+
+        lose = true;
     }
 }
